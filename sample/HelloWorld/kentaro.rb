fd1f2a96a875617b3ebb9a042dcb366bf40cdd29
Kentaro Fujiy <kentaro@manacadigital.com.br>
2020-03-24 02:19:41 +0000

    create two sessions

--- sample/HelloWorld/kentaro.rb
+++ sample/HelloWorld/kentaro.rb
@@ -3,40 +3,22 @@ require 'opentok'
 
 raise "You must define API_KEY and API_SECRET environment variables" unless ENV.has_key?("API_KEY") && ENV.has_key?("API_SECRET")
 
-class HelloWorld < Sinatra::Base
+class Kentaro < Sinatra::Base
 
   set :api_key, ENV['API_KEY']
   set :opentok, OpenTok::OpenTok.new(api_key, ENV['API_SECRET'])
   set :session, opentok.create_session
 
-  set :kapi_key, ENV['API_KEY']
-  set :kopentok, OpenTok::OpenTok.new(kapi_key, ENV['API_SECRET'])
-  set :ksession, kopentok.create_session
-
-  get '/' do
+  get '/kentaro' do
 
     api_key = settings.api_key
     session_id = settings.session.session_id
-    #session_id = OpenTok::OpenTok.new(api_key, ENV['API_SECRET']).create_session
     token = settings.opentok.generate_token(session_id)
 
     erb :index, :locals => {
       :api_key => api_key,
       :session_id => session_id,
       :token => token
-    }
-  end
- get '/kentaro' do
-
-    kapi_key = settings.kapi_key
-    ksession_id = settings.ksession.session_id
-    #session_id = OpenTok::OpenTok.new(api_key, ENV['API_SECRET']).create_session
-    ktoken = settings.kopentok.generate_token(ksession_id)
-
-    erb :index, :locals => {
-      :api_key => kapi_key,
-      :session_id => ksession_id,
-      :token => ktoken
     }
   end
 
